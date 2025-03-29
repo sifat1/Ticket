b3c653425f5a49ba3130fb9d8f8d5e1862441dd3
sifat1 <sifatshikdhar@gmail.com>
2025-03-29 14:43:35 +0600

    added tickets

--- obj/Debug/net8.0/Ticket.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ticket.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ticket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20cf529ff0002756e4b31985aee5e392e7a37dd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a26325033586b36df8eb6fbfbff4d789ff5179e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ticket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ticket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
